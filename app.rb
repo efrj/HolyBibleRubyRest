5822bcdc6e3bebf25bc39df9ca6d2f89434af20c
thunder <efrj@hotmail.com>
2016-11-10 10:14:04 -0200

    Add Book Chapter Reader route to app.rb

--- app.rb
+++ app.rb
@@ -16,6 +16,12 @@ get '/' do
     erb :index
 end
 
+get '/book/:book/chapter/:chapter' do
+    book = Bible.find_by(BookAbr: params['book'], Chapter: params['chapter'])
+    verses = Bible.where(["BookAbr = ?", params['book']]).where(["Chapter = ?", params['chapter']]).select("Verse as verse, VText as text").all
+    json :book => book.BookName, :chapter => book.Chapter, :verses => verses
+end
+
 get '/book/:book/chapter/:chapter/verse/:verse' do
     book = Bible.find_by(BookAbr: params['book'], Chapter: params['chapter'], Verse: params['verse'])
     json :book => book.BookName, :chapter => book.Chapter, :verse => book.Verse, :text => book.VText
